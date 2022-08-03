d86d9912cb7bf7748ce83da3a9325eff31d7d11d
menclp <94796668+menclp@users.noreply.github.com>
2022-08-03 09:43:50 +0200

    Update Person.php

--- class/Person.php
+++ class/Person.php
@@ -1,1 +1,18 @@
+<?php
 
+class Person
+{
+  public $id;
+  private $name;
+  private $surname;
+  private $dept;
+  
+  public function __construct($id, $name, $surname, $dept)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->surname = $surname;
+    $this->dept = $dept;
+  }
+  
+}
